4cfbd0554f99c9b167c5bcedd545dee8739c4e23
Brayan Heredia <76888464+bhm96@users.noreply.github.com>
2021-01-12 17:41:28 -0500

    Update javaS1.js

--- public/js/javaS1.js
+++ public/js/javaS1.js
@@ -3,6 +3,8 @@ jQuery('document').ready(function($){
     var menuBtn= $('.menu-icon'),
         menu=$('.navigation ul');
 
+    
+    
     menuBtn.click(function(){
         if(menu.hasClass('show')){
             menu.removeClass('show');
