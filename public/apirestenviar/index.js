258af010025d98f80fce7e4d84320faa963be802
Brayan Heredia <76888464+bhm96@users.noreply.github.com>
2021-01-27 14:14:39 -0500

    Update index.js

--- public/apirestenviar/index.js
+++ public/apirestenviar/index.js
@@ -5,7 +5,6 @@ const async = require("async");
 
 const { Pool } = require("pg");
 
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
